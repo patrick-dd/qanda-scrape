18a82fc86c390c370cd85d45abab6faed6b19618
patrick-dd <patrick.doupe@gmail.com>
2016-05-15 14:16:48 +1000

    first commit

---
+++ get_qanda_htmls.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+html_link = 'http://www.abc.net.au/tv/qanda/past-programs-by-date.htm'
+
+page = requests.get(html_link)
+soup = BeautifulSoup(page.text, 'lxml')
+
+f = open('qanda_episodes.txt', 'w')
+
+for link in soup.find_all('a'):
+    if 'http://www.abc.net.au/tv/qanda/txt/' in link.get('href'):
+        f.write(link.get('href'))
+        f.write(', ')
+
+f.close()
+print 'Websites scraped!'
+
+
+
